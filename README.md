f0abf9554982be72b6f69ada74990fedcb4b098e
tickBit <majuketo@gstudent.jyu.fi>
2023-05-30 16:04:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Toy spaCy experiment
+
+You'll need to have the spaCy module installed. In this project "en_core_web_lg" model was used, but you can use "en_core_web_sm" instead, if you want. To download the large model type
+
+* python -m spacy download en_core_web_lg
+
+## Using the program
+
+- Give a story to the system and ask "where" question about the objects in the story
+- The system understands only few verbs: go, travel, move, leave, drop, leave, get and take. Also the past tense of those verbs.
+
+I got inspired from the Babi dataset from Facebook research and have used as an example story the following: *Mary moved to the bathroom. Sandra took the apple. Mary went to the hallway. Mary got the milk. John moved to the bedroom. Mary dropped the milk. John took the milk. John moved to the kitchen. Sandra went to the bedroom.*
+
+## Example pictures of the program running
+
---
+++ Where.py
@@ -0,0 +1,83 @@
+import spacy
+nlp = spacy.load('en_core_web_lg')
+
+def get_question_object(question):
+
+    doc = nlp(question)
+    doc = [token for token in doc if token.dep_ == 'nsubj'] 
+
+    return doc[0].text
+
+while True:
+
+    context = input("Enter story: (press enter for default story) \n")
+
+    if context.strip() == "":
+        context = "Mary moved to the bathroom. Sandra took the apple. Mary went to the hallway. Mary got the milk. John moved to the bedroom. Mary dropped the milk. John took the milk. John moved to the kitchen. Sandra went to the bedroom."
+    else:
+        context = context.strip()
+
+    print("Story: ", context)
+    print()
+
+    while True:
+        question = input("Enter question: (eg. Where is the milk?) (Just enter to return to the story.)\n")
+
+        if question.strip() == "":
+            break
+
+        # dict that tells which object are related to which person and where they are
+        object = {}
+        place = {}
+
+        qst_obj = get_question_object(question)
+
+        print("Object in question: ", qst_obj)
+        print()
+
+        doc = nlp(context)
+
+        found = False
+        for w in doc:
+            if w.text == qst_obj: found = True
+        
+        if not found:
+            print("Object not found in the story. Please try again.")
+            print()
+            continue
+
+        for sent in doc.sents:
+           
+            for w in sent:
+
+                if w.lemma_ == "drop" or w.lemma_ == "leave":
+                    for w in sent:
+                        if w.dep_ == 'nsubj':
+                            subj = w.text
+
+                        if w.dep_ == 'dobj':
+
+                            currentPlace = place[subj]                 
+                            object[w.text] = "NOBODY"
+                            place["NOBODY"] = currentPlace
+                        
+                                
+                
+                if w.lemma_ == "go" or w.lemma_ == "travel" or w.lemma_ == "move":
+                    for w in sent:
+                        if w.dep_ == 'nsubj':
+                            subj = w.text
+                        if w.dep_ == 'pobj':
+                            place[subj] = w.text
+
+                if w.lemma_ == "take" or w.lemma_ == "get":
+                    for w in sent:
+                        if w.dep_ == 'nsubj':
+                            subj = w.text
+
+                        if w.dep_ == 'dobj':
+                            obj = w.text
+                            object[obj] = subj
+
+        print(f"Answer: Object {qst_obj.upper()} is in the", place[object[qst_obj]].upper(), f"with person {object[qst_obj].upper()}")
+    print()
